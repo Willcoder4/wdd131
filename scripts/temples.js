7fe5eded2c9e856425e76d87292f689b57000746
Willcoder4 <abijefegor@gmail.com>
2025-11-17 16:19:15 +0100

    update scripts/temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,7 @@
-const nav = document.querySelector('.navigation');
+const nav = document.querySelector('.menu-toggle');
+const menuItem = document.querySelector('.menu')
 
 nav.addEventListener('click', () => {
-    nav.classList.toggle('show');
+    nav.classList.toggle('active');
+    menuItem.classList.toggle('active');
 });
