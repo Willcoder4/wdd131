2a2fe451e552b131cadea7fe7bdc157b5ad3d354
Willcoder4 <abijefegor@gmail.com>
2026-01-08 14:58:02 +0100

    Deleted some files and updated my portfolio webpage.

--- week02/scripts/temples.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector('.menu-toggle');
-const menuItem = document.querySelector('.menu')
-
-nav.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    menuItem.classList.toggle('active');
-});
