8c175af910657e556444db5ef6bf895d2d4da5b6
Willcoder4 <abijefegor@gmail.com>
2026-03-06 12:46:53 +0100

    Updated Code

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,38 +1,7 @@
-// const date = new Date();
+const date = new Date();
 
-// // Current Year
-// const year = document.getElementById("currentyear").innerHTML = date.getFullYear();
+// Current Year
+const currentYear = document.getElementById("currentyear").innerHTML = date.getFullYear();
 
-// // Last Modified
-// const lastModified = document.getElementById("lastmodified").innerText = `Last Modified: ${date.toLocaleString()}`
-
-
-const DAYS = 6;
-const LIMIT = 30;
-let studentReport = [11, 42, 33, 64, 29, 37, 44];
-
-/* for (let i = 0; i < studentReport.length; i++) {
-    if (studentReport[i] < LIMIT) {
-        console.log(studentReport[i]);
-    }
-} */
-
-/* let i = 0;
-while (i < studentReport.length) {
-    if (studentReport[i] < LIMIT) {
-        console.log(studentReport[i]);
-    }
-    i++;
-} */
-
-/* studentReport.forEach(studentReports => {
-    if (studentReports < LIMIT) {
-        console.log(studentReports);
-    }
-}); */
-
-for (let i in studentReport) {
-    if (studentReport[i] < LIMIT) {
-        console.log(studentReport[i]);   
-    }
-}
+// Last Modified
+const lastModified = document.getElementById("lastmodified").innerText = `Last Modified: ${document.lastModified}`; //The lastModified property returns the date and time on which the current document was last modified
