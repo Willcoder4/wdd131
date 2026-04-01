6af2405009409d6b2e3b2c55b1c548ad6032df10
Willcoder4 <abijefegor@gmail.com>
2026-04-01 19:04:45 +0100

    Edited code

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -2,7 +2,7 @@ const temperature = 25; // °C
 const windSpeed = 10; // km/h
 
 function calculateWindChill(temp, speed) {
-  return Math.round(13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16)));
+  return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 }
 
 const windChillElement = document.querySelector("#windchill");
